caa9bd6b697369462c19860e1b87f5a049a7b321
david.hutto <david.hutto@msionline.com>
2020-01-08 16:57:52 -0600

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,6 +23,7 @@
                     <h1 class="aboutName">David Hutto</h1>
                     <h3 class="aboutTitle"> Web Developer & Designer</h3>
                     <p class="aboutPara">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    
                 </div>
             </b-col>
             <b-col cols="12" lg="6" class="aboutPic mt-5">
@@ -38,6 +39,10 @@
                 <div>
                     <h3>Frontend Development</h3>
                     <p class="aboutPara">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <b-badge pill class="skillPill mx-1 px-2" >Vue.js</b-badge>
+                    <b-badge pill class="skillPill mx-1 px-2" >React</b-badge>
+                    <b-badge pill class="skillPill mx-1 px-2" >Bootstrap</b-badge>
+                    <b-badge pill class="skillPill mx-1 px-2" >git</b-badge>
                 </div>
             </b-col>
             <b-col>
